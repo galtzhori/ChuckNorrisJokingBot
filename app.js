0593801b1da7b8a97754470352575f12910471e5
galx <gal.tzhori@mail.huji.ac.il>
2023-12-10 21:53:20 +0200

    improved

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+// let createError = require('http-errors');
+// let express = require('express');
+// let path = require('path');
+// let cookieParser = require('cookie-parser');
+// let logger = require('morgan');
 //
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+// let indexRouter = require('./routes/index');
+// let usersRouter = require('./routes/users');
 //
-// var app = express();
+// let app = express();
 //
 // // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -39,43 +39,85 @@
 // });
 //
 // module.exports = app;
+const {Telegraf, Scenes, session} = require('telegraf');
+// const { enter, leave } = Scenes.Stage;
 
-const { Telegraf } = require('telegraf');
+// const { Scenes } = require('telegraf');
+
+// import { Scenes } from "telegraf";
+// const { s } = require('telegraf/Scenes');
+
 const axios = require('axios').default;
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 // tokens of translator and bot
 let key = "e19e8ef57d4542c1af6e7ba2a88f0758";
 let endpoint = "https://api.cognitive.microsofttranslator.com";
-const botToken = '6706534572:AAHV4PmvziuVhyluQB86HPOKY8kClhMNDvE';
+let botToken = '6706534572:AAHV4PmvziuVhyluQB86HPOKY8kClhMNDvE';
 // location, also known as region.
 let location = "northeurope";
 
+const contactWizard = new Scenes.WizardScene(
+    'CONVERSATION_ID', // first argument is Scene_ID, same as for BaseScene
+    (ctx) => {
+        ctx.reply('choose a language: set language <language_name>');
+        ctx.wizard.state.data = {};
+        return ctx.wizard.next();
+    },
+    (ctx) => {
+        // validation example
+        let re = new RegExp("set language (?<lan>[a-zA-Z]+)", "g");
+        const match = ctx.message.text.match(re);
+        if (!match) {
+            ctx.reply('enter a legal language');
+            return;
+        }
+        if (match[0])
+            ctx.wizard.state.contactData.fio = ctx.message.text;
+        ctx.reply('Enter your e-mail');
+        return ctx.wizard.next();
+    },
+    async (ctx) => {
+        ctx.wizard.state.contactData.email = ctx.message.text;
+        ctx.reply('Thank you for your replies, well contact your soon');
+        await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData);
+        return ctx.scene.leave();
+    },
+);
+
 const bot = new Telegraf(botToken);
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.launch()
 
-let params = new URLSearchParams();
-params.append("api-version", "3.0");
-params.append("from", "en");
-params.append("to", "he");
+// // bot.start((ctx) => ctx.reply('Welcome'))
+// // bot.help((ctx) => ctx.reply('Send me a sticker'))
+const stage = new Scenes.Stage([contactWizard]);
+bot.use(session());
+bot.use(stage.middleware());
+bot.command('start', (ctx) => ctx.scene.enter('CONVERSATION_ID'));
+bot.launch();
 
-axios({
-  baseURL: endpoint,
-  url: '/translate',
-  method: 'post',
-  headers: {
-    'Ocp-Apim-Subscription-Key': key,
-    'Ocp-Apim-Subscription-Region': location,
-    'Content-type': 'application/json',
-    'X-ClientTraceId': uuidv4().toString()
-  },
-  params: params,
-  data: [{
-    'text': 'Hello, friend! What did you do today?'
-  }],
-  responseType: 'json'
-}).then(function(response){
-  console.log(JSON.stringify(response.data, null, 4));
-})
+function translate(strToTranslate, language) {
+    let params = new URLSearchParams();
+    params.append("api-version", "3.0");
+    params.append("from", "en");
+    params.append("to", language);
+
+    axios({
+        baseURL: endpoint,
+        url: '/translate',
+        method: 'post',
+        headers: {
+            'Ocp-Apim-Subscription-Key': key,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': uuidv4().toString()
+        },
+        params: params,
+        data: [{
+            'textToTranslate': strToTranslate
+        }],
+        responseType: 'json'
+    }).then(function (response) {
+        // console.log(JSON.stringify(response.data, null, 4));
+        console.log(response.data[0].text)
+    })
+}
